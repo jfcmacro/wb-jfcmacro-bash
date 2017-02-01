86e7315f3eaadcb406ee243de31638707315c829
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2017-02-01 16:08:08 +0000

    Adding new version cleanTexFile.sh

--- cleanTexFile.sh
+++ cleanTexFile.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 for i in $@
 do
     if [ -f $i ]
     then
-	yes | emacs "$i" --batch -l ~/lib/emacs-batch/cleanTeXFile.el
+	yes | emacs "$i" --user $USER --batch -l ~/lib/emacs-batch/cleanTeXFile.el 1>/dev/null 2>&1
     fi
 done
