8389f8cef5ab770c82bf023993756b2be3b1ebd0
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2017-02-28 16:22:49 +0000

    Adding Publicar

--- publicar.sh
+++ publicar.sh
@@ -1,27 +1,29 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-file=$1
+for i in $@
+do
+    file=$i
 
-if [ -f $file ]
-then
-
-    filename=`basename $file .pdf`
-    
-    course=`echo $filename | sed 's/-/ /g' | awk '{ print $1; }'`
-    year=`echo $filename | sed 's/-/ /g' | awk '{ print $2; }'`
-    term=`echo $filename | sed 's/-/ /g' | awk '{ print $3; }'`
-    
-    courselower=`echo $course | awk '{ print tolower($0) }'`
-
-    publishDir=$HOME/Dropbox/Cursos/$courselower/$year/$term/Presentaciones
-    
-    if [ ! -d $publishDir ]
+    if [ -f $file ]
     then
-	mkdir $publishDir
-	echo "Creating directory $publishDir"
-    fi
-else
-    echo "File: $file doesn't exits"
-fi
 
-cp $file $publishDir
+	filename=`basename $file .pdf`
+    
+	course=`echo $filename | sed 's/-/ /g' | awk '{ print $1; }'`
+	year=`echo $filename | sed 's/-/ /g' | awk '{ print $2; }'`
+	term=`echo $filename | sed 's/-/ /g' | awk '{ print $3; }'`
+    
+	courselower=`echo $course | awk '{ print tolower($0) }'`
+
+	publishDir=$HOME/Dropbox/Cursos/$courselower/$year/$term/Presentaciones
+    
+	if [ ! -d $publishDir ]
+	then
+	    mkdir $publishDir
+	    echo "Creating directory $publishDir"
+	fi
+	cp $file $publishDir
+    else
+	echo "File: $file doesn't exits"
+    fi
+done
